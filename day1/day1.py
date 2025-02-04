7209dde13a4880e473d733aad16cdad2a1833823
Shabbir-Anjum <mahrshabbir768@gmail.com>
2025-02-04 15:56:00 +0500

    update2

---
+++ day1/day1.py
@@ -0,0 +1,65 @@
+# # print("ad")
+# # print("hello word")
+# # print("hello word")
+
+
+# """
+# This is a comment
+# written in
+# more than just one line
+# print("hello word")
+# """
+
+# # x = 5
+# y = "John"
+# # print(x)
+# print(y)
+# q=6
+# print(q)
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+y=5
+y=7
+print(y)
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+myvar = "John"
+
+
+x, y, z,c = "Orange", "Banana", "Cherry","hello"
+print(z)
+x = y = z = "Orange"
+
+print(x)
+
+x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
+
+print("Python is " + x," hello")
+
+x = 5
+print(type(x))
+
+c="python"
+print(type(c))
+x=4
+x="python course "+ str(4)
+print(x)
+
+print(int(35.88))
+
+a = "Hello, World!"
+print(len(a))
+
+
+x = 'Welcome'
+print(x[3])
+
+b = "Hello, World!"
+print(b[:5])
