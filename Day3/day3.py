22c32e122c000101549761c7aeb19b0f94830e08
Shabbir-Anjum <mahrshabbir768@gmail.com>
2025-02-05 23:49:18 +0500

    update

---
+++ Day3/day3.py
@@ -0,0 +1,71 @@
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop(2)
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop()
+# print(thislist)
+
+
+# thislist = ["apple", "banana", "cherry"]
+# del thislist[1]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# del thislist
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.clear()
+# print(thislist)
+
+
+# thislist = ["apple", "banana", "cherry", "abc"]
+# for x in thislist:
+#   print(x)
+
+# thislist = ["apple", "banana", "cherry"]
+
+# print(range(len(thislist)))
+# for a in  range(len(thislist)):
+#     print(a)
+#     print(thislist[a])
+
+
+# # thislist = ["apple", "banana", "cherry"]
+# # for i in range(len(thislist)):
+# #   print(thislist[i])
+
+
+# thislist = ["apple", "banana", "cherry"]
+
+# # print(thislist[0])
+# # print(thislist[1])
+# # print(thislist[2])
+# x=3
+# while x < len(thislist):
+#     print(thislist[x])
+#     x=x+1
+
+# for x in ['apple', 'banana', 'cherry']:
+#   print(x)
+
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+# for b in fruits:
+# #    print(b)
+#    if "a" in b:
+#         newlist.append(b)
+
+# print(newlist)
+
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if x != "apple"]
+print(newlist)
+
+
+newlist = [x for x in range(10) if x < 9]
+
+print(newlist)
