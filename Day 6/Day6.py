7f9cae54c8045ef295b66351775eeec2b1ce3284
Shabbir-Anjum <mahrshabbir768@gmail.com>
2025-02-11 22:46:25 +0500

    update

---
+++ Day 6/Day6.py
@@ -0,0 +1,104 @@
+# thisset = {"apple", "banana", "cherry"}
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry", "apple"}
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+# print(thisset)
+
+
+# thisset = {"apple", "banana", "cherry"}
+
+# for x in thisset:
+#   print(x)
+
+
+# thisset = {"apple", "banana", "cherry"}
+
+# print("banana" in thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+
+# print("banana" not in thisset)
+
+
+# thisset = {"apple", "banana", "cherry"}
+
+# thisset.add("orange")
+
+# print(thisset)
+
+
+# thisset = {"apple", "banana", "cherry"}
+# tropical = {"pineapple", "mango", "papaya"}
+
+# thisset.update(tropical)
+
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+# mylist = ["kiwi", "orange"]
+
+# thisset.update(mylist)
+
+# print(thisset)
+
+
+# thisset = {"apple", "banana", "cherry"}
+
+# thisset.remove("banana")
+
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+
+# thisset.discard("banana")
+
+# print(thisset)
+
+
+# thisset = {"apple", "banana", "cherry"}
+
+# x = thisset.pop()
+
+# print(x)
+
+# print(thisset)
+
+
+# thisset = ["apple", "banana", "cherry"]
+
+# thisset.delete("apple")
+
+# print(thisset)
+
+
+# set1 = {"a", "b", "c", 1, 2}
+# set2 = {1, 2, 3}
+
+# set3 = set1.union(set2)
+# print(set3)
+
+
+# set1 = {"a", "b", "c"}
+# set2 = {1, 2, 3}
+
+# set3 = set1 | set2
+# print(set3)
+
+
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+
+# set3 = set1.intersection(set2)
+# print(set3)
+
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+
+# set3 = set2.difference(set1)
+
+# print(set3)
+
---
+++ Day5/Day5.py
@@ -0,0 +1,94 @@
+# mytuple = ("apple", "banana", "cherry",2, 2.2)
+
+# print(mytuple[0])
+
+
+# thistuple = ("apple", "banana", "cherry")
+# print(len(thistuple))
+
+# mytuple = ("apple", "banana", "cherry")
+# print(type(mytuple))
+
+# value=["apple", "banana", "cherry"]
+# thistuple = tuple(value) # note the double round-brackets
+# print(thistuple)
+# print(type(thistuple))
+
+
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple[-1])
+
+
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(thistuple[2:5])
+
+
+# x = ("apple", "banana", "cherry")
+# y = list(x)
+# print(y)
+# y[0] = "kiwi"
+# x = tuple(y)
+
+# print(x)
+
+
+# thistuple = ("apple", "banana", "cherry")
+# y = ("orange",)
+# thistuple =thistuple + y
+
+# print(thistuple)
+
+# thistuple = ("apple", "banana", "cherry")
+
+# del thistuple
+# print(thistuple) #this will raise an error because the tuple no longer exists
+
+
+
+# fruits = ("apple", "banana", "cherry")
+
+# (green, yellow, red) = fruits
+
+# print(green)
+# print(yellow)
+# print(red)
+
+
+
+# fruits = ("apple","dd", "banana", "cherry", "strawberry", "raspberry","value")
+
+# (green, yellow, *red) = fruits
+
+# print(green)
+# print(yellow)
+# print(red)
+
+
+
+# thistuple = ("apple", "banana", "cherry")
+# for x in thistuple:
+#   print(x)
+
+
+tuple1 = ("a", "b" , "c")
+tuple2 = ("r","3")
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+
+# list1 = ["a", "b" , "c"]
+# list2 = [1, 2, 3]
+
+# list1.extend(list2)
+# print(list1)
+
+
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
+
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+print(thisset)
