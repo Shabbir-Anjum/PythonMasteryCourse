ba3857a4fc9b8f25c35e9ef64300f07634f72cb6
Shabbir-Anjum <mahrshabbir768@gmail.com>
2025-02-18 22:49:57 +0500

    updatw

---
+++ Day10/day10.py
@@ -0,0 +1,74 @@
+# i = 1
+# while 6 < 6:
+#   print(2)
+#   i = 5+1
+# print("value")
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   if i == 1:
+#     break
+#   i += 1
+# print("break")
+
+# i = 0
+# while i < 6:
+#   i += 1
+#   if i == 1:
+#     continue
+#   print(i)
+  
+# print("hello")
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+# else:
+#   print("i is no longer less than 6")
+
+# fruits = ["apple", "banana", "cherry",'gf']
+# for z in fruits:
+#   print(z)
+# for x in "banana":
+#   print(x)
+# fruits = ["apple", "banana", "cherry",'gfhg']
+# for x in fruits:
+#   print(x)
+#   if x == "banana":
+#     break
+# fruits = ["apple", "banana", "cherry",'gfhg']
+# for x in fruits:
+#   if x == "banana":
+#     continue
+#   print(x)
+# print("point")
+
+
+# for x in range(10):
+#   print(x)
+# for x in range(2, 6):
+#   print(x)
+# for x in range(2, 30, 5):
+#   print(x)
+
+# for x in range(6):
+#   print(x)
+# else:
+#   print("Finally finished!")
+
+# for x in range(6):
+#   if x == 3: break
+#   print(x)
+# else:
+#   print("Finally finished!")
+
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+
+# for x in adj:
+#   for y in fruits:
+#     print(x, y)
+for x in [0, 1, 2]:
+    if x== 0: break
